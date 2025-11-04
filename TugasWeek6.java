c1b21fb21970b982a2124adff0d101c02cf3b297
Aliesy1628 <gulaku1628@gmail.com>
2025-11-04 17:32:05 +0700

    Inputan nilai matkul

--- TugasWeek6.java
+++ TugasWeek6.java
@@ -5,5 +5,17 @@ public class TugasWeek6 {
         Scanner nscan = new Scanner(System.in);
         int nilaiSData, nilaiPBObjek, nilaiBData, nilaiAlgoPemrograman, nilaiRPL;
         double rataRata, ipk;
+
+        System.out.print("- Masukkan nilai Struktur Data: ");
+        nilaiSData = nscan.nextInt();
+        System.out.print("- Masukkan nilai Pemrograman Berorientasi Objek: ");
+        nilaiPBObjek = nscan.nextInt();
+        System.out.print("- Masukkan nilai Basis Data: ");
+        nilaiBData = nscan.nextInt();
+        System.out.print("- Masukkan nilai Algoritma dan Pemrograman: ");
+        nilaiAlgoPemrograman = nscan.nextInt();
+        System.out.print("- Masukkan nilai Rekayasa Perangkat Lunak: ");
+        nilaiRPL = nscan.nextInt();
+
     }
 }
