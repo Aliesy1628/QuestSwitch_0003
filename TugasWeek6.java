9649d08aeec2d80d5a544452e80583a2428d6035
Aliesy1628 <gulaku1628@gmail.com>
2025-11-04 17:31:28 +0700

    penambahan variabel

--- TugasWeek6.java
+++ TugasWeek6.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+
 public class TugasWeek6 {
-    
+    public static void main(String[] args) {
+        Scanner nscan = new Scanner(System.in);
+        int nilaiSData, nilaiPBObjek, nilaiBData, nilaiAlgoPemrograman, nilaiRPL;
+        double rataRata, ipk;
+    }
 }
